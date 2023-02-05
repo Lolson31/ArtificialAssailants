dea56be81d48f7d174d1d196522ee489d8f0073e
Luke Olson <lolson31@huskers.unl.edu>
2023-02-05 09:57:20 -0600

    Scene Change

--- Assets/Scripts/LoadFight.cs
+++ Assets/Scripts/LoadFight.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class PromptDisplay : MonoBehaviour
+public class LoadFight : MonoBehaviour
 {
-    public ImageRetriever imageRetriever;
-    private InputField input;
     // Start is called before the first frame update
     void Start()
     {
-        input= GetComponent<InputField>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        imageRetriever.input = input.text;
+        
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene("SampleScene");
     }
 }
